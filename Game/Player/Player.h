67654a4832365f27228415abe23bfa9e7adb1b99
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-07-09 15:41:05 +0900

    最低限のプレイヤー実装

---
+++ Game/Player/Player.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Adapter.h>
+
+/// <summary>
+/// プレイヤークラス
+/// </summary>
+class Player final {
+public: // ** メンバ関数 ** //
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Init();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	
+	/// <summary>
+	/// ワールド座標を取得
+	/// </summary>
+	/// <returns></returns>
+	LWP::Math::Vector3 GetWorldPosition() { model_.worldTF.GetWorldPosition(); }
+
+private: //*** メンバ変数 ***//
+
+	// モデル
+	LWP::Resource::RigidModel model_;
+};
