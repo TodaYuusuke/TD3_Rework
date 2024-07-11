e5891974ecdd8cb254046414767912357410a477
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-07-11 22:47:47 +0900

    移動入力を実装

--- Game/Object/Player/Player.cpp
+++ Game/Object/Player/Player.cpp
@@ -8,13 +8,112 @@ using namespace LWP::Utility;
 
 void Player::Init() {
 	model_.LoadCube();
+	// 関数ポインタ配列の初期化
+	InitStateFunctions();
 }
 
 void Player::Update() {
-	// すぐに消す
-	ImGui::Begin("player");
 
-	model_.worldTF.DebugGUI();
+	//*** 操作入力を受け取る ***//
 
-	ImGui::End();
+	// 移動、向き操作
+	CheckInputMove();
+
+	// 新しい状態になっていた時
+	if (reqBehavior_) {
+		behavior_ = reqBehavior_.value();
+		// 状態の初期化
+		(this->*stateInit_[(int)behavior_])();
+		reqBehavior_ = std::nullopt;
+	}
+
+	// 状態の更新
+	(this->*stateUpdate_[(int)behavior_])();
+
 }
+
+void Player::CheckInputMove() {
+	// 方向を取得
+	lwp::Vector2 direct{ 0.0f,0.0f };
+	if (lwp::Keyboard::GetPress(DIK_W)) {
+		direct.y += 1.0f;
+	}
+	else if (lwp::Keyboard::GetPress(DIK_S)) {
+		direct.y -= 1.0f;
+	}
+	if (lwp::Keyboard::GetPress(DIK_A)) {
+		direct.x -= 1.0f;
+	}
+	else if (lwp::Keyboard::GetPress(DIK_D)) {
+		direct.x += 1.0f;
+	}
+	direct += LWP::Input::Controller::GetLStick();
+	direct = direct.Normalize();
+
+	// そもそも移動入力が無かったらフラグを立てない
+	flags_.isInputMove = !(direct.x == 0 && direct.y == 0);
+
+	// 移動入力があった時に方向を更新する
+	// 方向をゼロにしない
+	if (flags_.isInputMove) {
+		// ここで三次元空間に変換
+		destinate_.x = direct.x;
+		destinate_.z = direct.y;
+	}
+}
+
+void Player::InitStateFunctions() {
+	// 初期関数を格納
+	stateInit_[(int)Behavior::Idle] = &Player::InitIdle;
+	stateInit_[(int)Behavior::Move] = &Player::InitMove;
+	stateInit_[(int)Behavior::Attack] = &Player::InitAttack;
+
+
+	// 更新関数を格納
+	stateUpdate_[(int)Behavior::Idle] = &Player::UpdateIdle;
+	stateUpdate_[(int)Behavior::Move] = &Player::UpdateMove;
+	stateUpdate_[(int)Behavior::Attack] = &Player::UpdateAttack;
+}
+
+#pragma region 状態の初期化
+
+void Player::InitIdle() {
+}
+
+void Player::InitMove() {
+}
+
+void Player::InitAttack() {
+}
+
+#pragma endregion
+
+
+#pragma region 状態の更新
+
+void Player::UpdateIdle() {
+	// 移動入力がされている時
+	if (flags_.isInputMove) {
+		reqBehavior_ = Behavior::Move;
+	}
+}
+
+void Player::UpdateMove() {
+	// 移動入力されている時
+	if (flags_.isInputMove) {
+		// 速度を代入する
+		velocity_ = destinate_ * Info::GetDeltaTimeF();
+	}
+	// 入力をやめた時
+	else {
+		velocity_ = { 0.0f,0.0f,0.0f };
+		reqBehavior_ = Behavior::Idle;
+	}
+
+	model_.worldTF.translation += velocity_;
+}
+
+void Player::UpdateAttack() {
+}
+
+#pragma endregion
--- Game/Object/Player/Player.h
+++ Game/Object/Player/Player.h
@@ -16,7 +16,7 @@ public: // ** メンバ関数 ** //
 	/// </summary>
 	void Update();
 
-	
+
 	/// <summary>
 	/// ワールド座標を取得
 	/// </summary>
@@ -25,11 +25,18 @@ public: // ** メンバ関数 ** //
 
 private: //*** サブクラス ***//
 
-	enum Behavior
-	{
+	// 状態
+	enum class Behavior : int {
 		Idle = 0,
 		Move,
 		Attack,
+
+		_Count,	// 状態最大数
+	};
+
+	// フラグをまとめた構造体
+	struct Flags {
+		bool isInputMove = false;
 	};
 
 private: //*** メンバ変数 ***//
@@ -37,7 +44,59 @@ private: //*** メンバ変数 ***//
 	// モデル
 	LWP::Resource::RigidModel model_;
 
-	Behavior behavior_ = Idle;
+	// 今の状態を格納
+	Behavior behavior_ = Behavior::Idle;
+	// 状態が変わった時に値が入る
 	std::optional<Behavior> reqBehavior_ = std::nullopt;
+
+	// 初期化関数ポインタの配列
+	void (Player::* stateInit_[(int)Behavior::_Count])();
+	// 更新関数ポインタの配列
+	void (Player::* stateUpdate_[(int)Behavior::_Count])();
+	
+private: //*** 細かく設定される変数 ***//
+
+	// まとめられたフラグ
+	Flags flags_;
+
+	// 次に移動する方向 | 向いている方向
+	// モデルの向きから判断すればいいかもしれない
+	// でも違う方向に向かう時に使うかも
+	// ↑別の変数に役割を分ければいい
+	LWP::Math::Vector3 destinate_ = { 0.0f,0.0f,1.0f };
+
+	// プレイヤーの速度を格納
+	LWP::Math::Vector3 velocity_ = { 0.0f,0.0f,0.0f };
+
+
+private: //*** メンバ関数 ***//
+
+	// 移動方向、向いている方向の入力を受け取る
+	void CheckInputMove();
+
+	// 関数ポインタ配列を初期化
+	void InitStateFunctions();
+
+private: //*** 各状態の関数群 ***//
+
+	// 状態の初期化
+
+	// 待機状態
+	void InitIdle();
+	// 移動状態
+	void InitMove();
+	// 攻撃状態
+	void InitAttack();
+
+
+	// 状態の更新
+
+	// 待機状態
+	void UpdateIdle();
+	// 移動状態
+	void UpdateMove();
+	// 攻撃状態
+	void UpdateAttack();
+
 
 };
