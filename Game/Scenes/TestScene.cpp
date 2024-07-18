38d5156aa3f546701dffcc99ebe4e8300a5cfe59
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-07-18 10:29:17 +0900

    Test用のシーン作成

---
+++ Game/Scenes/TestScene.cpp
@@ -0,0 +1,14 @@
+#include "TestScene.h"
+
+#include "Game/Play/PlayScene.h"
+
+using namespace LWP;
+using namespace LWP::Input;
+using namespace LWP::Primitive;
+using namespace LWP::Math;
+using namespace LWP::Utility;
+
+
+void TestScene::Initialize() {}
+
+void TestScene::Update() {}
---
+++ Game/Scenes/TestScene.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <scene/IScene.h>
+//#include "../../SceneTransition/SceneTransition.h"
+
+class TestScene final
+	: public IScene
+{
+public:
+	TestScene() = default;	// コンストラクタは自由に定義可能
+	~TestScene() = default;
+
+	//*** 純粋仮想関数の実体宣言 ***//
+
+	// 初期化
+	void Initialize() override;
+	// 更新
+	void Update() override;
+
+	// scene遷移したい場合、ISceneポインタ型のnextSceneに次のシーンの実体を代入
+
+private: //*** これより先に必要な処理や変数を記述 ***//
+
+public: //*** 関数群 ***//
+
+private: //*** 変数群 ***//
+};
